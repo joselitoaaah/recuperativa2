57dff5de93469a132845d45e0f80b2a9ae235eca
joselitoaaah <jose.fuentes10@virginiogomez.cl>
2024-11-04 23:36:23 -0300

    Avance N°2

--- app/src/main/java/cl/virginiogomez/recuperativa2/MainActivity3.java
+++ app/src/main/java/cl/virginiogomez/recuperativa2/MainActivity3.java
@@ -1,9 +1,6 @@
 package cl.virginiogomez.recuperativa2;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,23 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity2 extends AppCompatActivity {
+public class MainActivity3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main2);
-
-            Button botonagregar = findViewById(R.id.boton_agregar);
-            botonagregar.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
-                    startActivity(intent);
-                }
-            });
-
+        setContentView(R.layout.activity_main3);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
